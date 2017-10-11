3fed831239a0e628d96ad4c9306c6d8acf49c9fd
Jorefice <jorefice@iastate.edu>
2017-10-11 13:41:35 -0500

    Generates a single, complete, sudoku

--- sudoku.js
+++ sudoku.js
@@ -12,9 +12,13 @@ function generateSudoku(sudoku)
 	fillFirstGrid(sudoku);
 	fillSecondGrid(sudoku);
 	fillThirdGrid(sudoku);
+	fillFirstCol(sudoku);
+	printSudoku(sudoku);
+	console.log('backtracking...');
+	backtrack(sudoku)//, 3, 1);
 	
-	
-	console.log(sudoku);
+	// console.log(sudoku);
+	printSudoku(sudoku);
 }
 
 function initializeSudoku(sudoku)
@@ -46,7 +50,7 @@ function fillFirstGrid(sudoku)
 			//probably change check row/col/grid to update and then since we'll only be pulling
 			//values from options we shouldn't have to check if it's valid or not
 			// removeValue(sudoku[i][j+3].options, vals[index])
-			updateSudoku(sudoku, i, j, vals[index]);
+			updateSudoku(sudoku, i, j, vals[index], true);
 			remove(vals, index);
 		}
 	}
@@ -63,7 +67,16 @@ function fillSecondGrid(sudoku)
 			var options = sudoku[i][j].options;
 			var val = options[randomInt(options.length - 1)];
 			sudoku[i][j].val = val;
-			updateSudoku(sudoku, i, j, val);
+			updateSudoku(sudoku, i, j, val, true);
+			var temp = sudoku[2][3];
+			if(i < 2 && sudoku[2][3].options.length < 3)
+			{
+				console.log("fixed a thing?");
+				sudoku[i][j].val = null;
+				updateSudoku(sudoku, i, j, val, false);
+				removeValue(sudoku[i][j].options, val);
+				j--;
+			}
 		}
 	}
 }
@@ -78,11 +91,107 @@ function fillThirdGrid(sudoku)
 			var options = sudoku[i][j].options;
 			var val = options[randomInt(options.length - 1)];
 			sudoku[i][j].val = val;
-			updateSudoku(sudoku, i, j, val);
+			updateSudoku(sudoku, i, j, val, true);
 		}
 	}
 }
 
+function fillFirstCol(sudoku)
+{
+	for(var i = 3; i < 9; i++)
+	{
+		sudoku[i][0].val = sudoku[i][0].options[randomInt(8-i)];
+		updateSudoku(sudoku, i, 0, sudoku[i][0].val, true);
+	}
+}
+
+//Instead of going top to bottom, find the element with the fewest optinos and fill that in
+function backtrack(sudoku)
+{
+	// if(col > 8)
+	// {
+	// 	col = 0;
+	// 	row++;
+	// 	if(row > 8)
+	// 	{
+	// 		return true; //figure out what to return here
+	// 	}
+	// }
+	var cell = findCell(sudoku)
+	var row = cell[0];
+	var col = cell[1];
+	if(row == null)
+	{
+		return true;
+	}
+	// if(sudoku[row][col].val == null)
+	else
+	{
+		var options = sudoku[row][col].options;
+		var origOptions = [];
+		for(var i = 0; i < options.length; i++)
+		{
+			origOptions[i] = options[i];
+		}
+		for(var i = 0; i < options.length; i++)
+		{
+			//get all the possible options for this cell
+			val = sudoku[row][col].options[randomInt(options.length - 1)];
+			sudoku[row][col].val = val;
+			// printSudoku(sudoku);
+			//update the options for other cells
+			updateSudoku(sudoku, row, col, val, true);
+			//check that we have at least one options for all other cells
+			if(validateSudoku(sudoku))
+			{
+				//fill the next cell
+				var ret = backtrack(sudoku)//, row, col+1);
+				//if this returns true, then we successfully filled the board so we can return it
+				if(ret)
+				{
+					return true;
+				}
+			}
+			//if the sudoku wasn't validated or leads to an incomplete board later on, try the next value
+			updateSudoku(sudoku, row, col, val, false);
+			sudoku[row][col].val = null;
+			removeValue(sudoku[row][col].options, val);
+			removeValue(options, val);
+		}
+
+		sudoku[row][col].options = origOptions;
+		return false;
+	}
+	// else
+	// {
+	// 	backtrack(sudoku)//, row, col+1);
+	// }
+}
+
+function findCell(sudoku)
+{
+	var min = 10;
+	var row = null, col = null;
+
+	for(var i = 0; i < 9; i++)
+	{
+		for(var j = 0; j < 9; j++)
+		{
+			if(sudoku[i][j].val == null)
+			{
+				var options = sudoku[i][j].options;
+				if(options.length < min && options.length > 0)
+				{
+					min = options.length;
+					row = i;
+					col = j;
+				}
+			}
+		}
+	}
+
+	return [row, col];
+}
 
 //Cell object to represent each cell in the Sudoku board
 function cell(value, input, options)
@@ -118,42 +227,48 @@ function randomInt(max)
 	return Math.floor(Math.random() * (max + 1));
 }
 
-function updateSudoku(sudoku, row, col, val)
+function updateSudoku(sudoku, row, col, val, adding)
 {
-	updateRow(sudoku, row, val);
-	updateCol(sudoku, col, val);
-	updateGrid(sudoku, row, col, val);
+	updateRow(sudoku, row, val, adding);
+	updateCol(sudoku, col, val, adding);
+	updateGrid(sudoku, row, col, val, adding);
 }
 
-function updateRow(sudoku, row, val)
+function updateRow(sudoku, row, val, adding)
 {
 	for(var i = 0; i < 9; i++)
 	{
-		// if(sudoku[row][i] == val)
-		// {
-		// 	return false;
-		// }
-		removeValue(sudoku[row][i].options, val);
+		if(adding)
+		{
+			removeValue(sudoku[row][i].options, val);
+		}
+		else
+		{
+			sudoku[row][i].options.push(val);
+		}
 	}
 
 	return true;
 }
 
-function updateCol(sudoku, col, val)
+function updateCol(sudoku, col, val, adding)
 {
 	for(var i = 0; i < 9; i++)
 	{
-		// if(sudoku[i][col] == val)
-		// {
-		// 	return false;
-		// }
-		removeValue(sudoku[i][col].options, val);
+		if(adding)
+		{
+			removeValue(sudoku[i][col].options, val);
+		}
+		else
+		{
+			sudoku[i][col].options.push(val);
+		}
 	}
 
 	return true;
 }
 
-function updateGrid(sudoku, row, col, val)
+function updateGrid(sudoku, row, col, val, adding)
 {
 	//sorry for this but I just didn't want like 20 lines to set 2 variables
 	var r = (row < 3)? 0 : (row < 6)? 3 : 6;
@@ -163,12 +278,65 @@ function updateGrid(sudoku, row, col, val)
 	{
 		for(var j = 0; j < 3; j++)
 		{
-			// if(sudoku[i][j] == val)
-			// {
-			// 	return false;
-			// }
-			removeValue(sudoku[i][j].options, val);
+			if(adding)
+			{
+				removeValue(sudoku[r+i][c+j].options, val);
+			}
+			else
+			{
+				sudoku[r+i][c+j].options.push(val);
+			}
 		}
 	}
 	return true;
+}
+
+function validateSudoku(sudoku)
+{
+	for(var i = 0; i < 9; i++)
+	{
+		for(var j = 0; j < 9; j++)
+		{
+			// var temp = sudoku[i][j];
+			//if we don't already have a value in a location and we have no potential values there, we have na invalid board
+			if(sudoku[i][j].val == null && sudoku[i][j].options.length == 0)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+function printSudoku(sudoku)
+{
+	for(var i = 0; i < 9; i++)
+	{
+		var row = "";
+		for(var j = 0; j < 9; j++)
+		{
+			var val = sudoku[i][j].val;
+			if(val == undefined)
+			{
+				row += "_ ";
+			}
+			else
+			{
+				row += val.toString() + " ";
+			}
+
+			if(j % 3 == 2)
+			{
+				row += " | ";
+			}
+		}
+
+		console.log(row);
+		if(i % 3 == 2)
+		{
+			console.log("---------------------------");
+		}
+	}
+	console.log(' ');
 }
